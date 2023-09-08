82e9b7b66e8c4f8b8fbc97a21726f7286ebffc3f
HoanVirtue <taduchoan5503@gmail.com>
2023-09-08 23:12:02 +0700

    test_first

--- Program.cs
+++ Program.cs
@@ -3,6 +3,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+// tesst
+builder.Services.AddControllers();
+// end tesst
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -23,5 +28,6 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
 
 app.Run();
