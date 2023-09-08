74b04460e07793bea85144fd394faec2c6ebad7a
HoanVirtue <taduchoan5503@gmail.com>
2023-09-08 23:47:37 +0700

    add-commit

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-// branch hoan
+// branch hoan 1
 
 var app = builder.Build();
 
