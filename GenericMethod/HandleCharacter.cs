dcf19d4b2c8aeb476192ffb321f624d04dc225df
HoanVirtue <taduchoan5503@gmail.com>
2023-09-14 16:53:00 +0700

    xong_layout

---
+++ GenericMethod/HandleCharacter.cs
@@ -0,0 +1,59 @@
+namespace HHSocialNetwork_Project.GenericMethod
+{
+    public class HandleCharacter
+    {
+        private static HandleCharacter instance;
+        public static HandleCharacter Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new HandleCharacter();
+                return instance;
+            }
+            private set => instance = value;
+        }
+
+        private HandleCharacter()
+        {
+        }
+        
+        public bool IsSpecialChar(string characters)
+        {
+            string specialCharacters = "!@#$%^&*()";
+            foreach(char c in characters)
+            {
+                if(specialCharacters.Contains(c))
+                    return true;
+            }
+            return false;
+        }
+
+        public bool IsUpperCaseChar(string characters)
+        {
+            foreach(char c in characters) {
+                if(char.IsUpper(c))
+                    return true;
+            }
+            return false;
+        }
+
+        public bool IsLowerCaseChar(string characters)
+        {
+            foreach(char c in characters) {
+                if(char.IsLower(c))
+                    return true;
+            }
+            return false;
+        }
+
+        public bool IsDigitChar(string characters)
+        {
+            foreach(char c in characters) {
+                if(char.IsDigit(c))
+                    return true;
+            }
+            return false;
+        }
+    }
+}
--- wwwroot/js/users.js
+++ wwwroot/js/users.js
@@ -1,36 +1,70 @@
-$(document).ready(function() {
-    $('#btnRegister').on('click', function() {
+$(document).ready(function () {
+    $('#btnRegister').on('click', function () {
         var formData = new FormData();
         formData.append("FirstName", $('#firstName').val());
         formData.append("LastName", $('#lastName').val());
         formData.append("Email", $('#email').val());
         formData.append("Password", $('#password').val());
         formData.append("RetyePassword", $('#retyptePassword').val());
+        formData.append("Birthday", $('#birthday').val());
         formData.append("GenderName", $('input[name="GenderName"]:checked').val());
-        
-        if(formData.get("FirstName"))
-        $.ajax({
-            url: '/Users/Register',
-            type: 'POST',
-            contentType: false,
-            processData: false,
-            cache: false,
-            data: formData,
-            success: function(response) {
-                if(response.success) {
-                    console.log('Add thanh cong');
-                } else {
-                    console.log('Error them: ', response.errors[0]);
+
+        if (formData.get("FirstName"))
+            $.ajax({
+                url: '/Users/Register',
+                type: 'POST',
+                contentType: false,
+                processData: false,
+                cache: false,
+                data: formData,
+                success: function (response) {
+                    if (response.success) {
+                        console.log('Add thanh cong');
+                    } else {
+                        var regError = document.getElementById("reg_error_inner");
+                        if (regError !== null) {
+                            var message = "";
+                            for (var i = 0; i < response.errors.length; i++) {
+                                message += response.errors[i];
+                                if (i < response.errors.length - 1) {
+                                    message += "<br>";
+                                }
+                            }
+                            regError.innerHTML = message;
+                            
+                        } else {
+                            var divReg = document.createElement("div");
+                            divReg.id = "reg_error";
+                            divReg.classList.add("_58mn", "message_error_register");
+                            divReg.role = "alert";
+                            divReg.tabIndex = -1;
+                            var divItem = document.createElement("div");
+                            divReg.id = "reg_error_inner";
+                            divReg.classList.add("_58mo");
+                            divReg.tabIndex = 0;
+                            var message = "";
+                            for (var i = 0; i < response.errors.length; i++) {
+                                message += response.errors[i];
+                                if (i < response.errors.length - 1) {
+                                    message += "<br>";
+                                }
+                            }
+                            divItem.innerHTML = message;
+                            divReg.appendChild(divItem);
+
+                            var modal = $('#registerModal').find('.modal-body');
+                            modal.prepend(divReg);
+                        }
+                    }
+                },
+                error: function (error) {
+                    console.log("Users Error: ", error);
                 }
-            },
-            error: function(error) {
-                console.log("Users Error: ", error);
-            }
-        })
+            })
     });
 
     function validateData(data) {
-        if(data == "") {
+        if (data == "") {
             console.log('Vui lòng nhập trường ...');
         }
     }
