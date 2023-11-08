e00afe51dfb622827233b122b3d297dac4ba406f
HoanVirtue <taduchoan5503@gmail.com>
2023-11-09 02:23:14 +0700

    'search_09_11'

---
+++ Controllers/SearchsController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Clone_Main_Project_0710.Controllers
+{
+    public class SearchsController : Controller
+    {
+        public IActionResult All()
+        {
+            return View();
+        }
+    }
+}
